1267bedc4434eb29a499a0ebc72126ebdec55bc6
JS <25235048+vertigozero1@users.noreply.github.com>
2020-06-27 20:31:24 -0500

    reduced fast encoder spin rate

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -934,8 +934,8 @@
 // Change values more rapidly when the encoder is rotated faster
 #define ENCODER_RATE_MULTIPLIER
 #if ENABLED(ENCODER_RATE_MULTIPLIER)
-  #define ENCODER_10X_STEPS_PER_SEC   30  // (steps/s) Encoder rate for 10x speed
-  #define ENCODER_100X_STEPS_PER_SEC  80  // (steps/s) Encoder rate for 100x speed
+  #define ENCODER_10X_STEPS_PER_SEC   20  // (steps/s) Encoder rate for 10x speed
+  #define ENCODER_100X_STEPS_PER_SEC  50  // (steps/s) Encoder rate for 100x speed
 #endif
 
 // Play a beep when the feedrate is changed from the Status Screen
