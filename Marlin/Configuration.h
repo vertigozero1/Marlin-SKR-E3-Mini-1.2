83dfc02a74e0a63050aa66667ebc69dbb6994e84
JS <25235048+vertigozero1@users.noreply.github.com>
2020-06-27 20:22:16 -0500

    enabled m503

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1444,7 +1444,7 @@
  *   M502 - Revert settings to "factory" defaults. (Follow with M500 to init the EEPROM.)
  */
 #define EEPROM_SETTINGS     // Persistent storage with M500 and M501
-#define DISABLE_M503        // Saves ~2700 bytes of PROGMEM. Disable for release!
+//#define DISABLE_M503        // Saves ~2700 bytes of PROGMEM. Disable for release!
 #define EEPROM_CHITCHAT       // Give feedback on EEPROM commands. Disable to save PROGMEM.
 #if ENABLED(EEPROM_SETTINGS)
   //#define EEPROM_AUTO_INIT  // Init EEPROM automatically on any errors.
